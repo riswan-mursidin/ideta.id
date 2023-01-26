11ad134c36f928dd9e5df29fade533d58bbd394c
Riswan Mursidin <achmad.riswan@gmail.com>
2023-01-26 22:38:04 +0800

    update No Whatsapp

--- scripts.js
+++ scripts.js
@@ -4,6 +4,6 @@ function sendWhatsApp() {
     "Halo idetrans, saya dari website anda dan tertarik dengan tawaran bisnis anda, hubungi saya kembali di kontak " +
     name;
   var whatsappUrl =
-    "https://api.whatsapp.com/send?phone=6285255930077&text=" + message;
+    "https://api.whatsapp.com/send?phone=6289616930077&text=" + message;
   window.location.href = whatsappUrl;
 }
